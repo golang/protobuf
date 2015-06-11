3b2a189289f42ac2f390127c93a4bc16bb37d059
Tamir Duberstein <tamird@gmail.com>
2015-06-11 07:39:33 -0400

    Add travis config
    Make sure generated code doesn't drift

--- protoc-gen-go/plugin/plugin.pb.go
+++ protoc-gen-go/plugin/plugin.pb.go
@@ -3,7 +3,7 @@
 // DO NOT EDIT!
 
 /*
-Package google_protobuf_compiler is a generated protocol buffer package.
+Package plugin_go is a generated protocol buffer package.
 
 It is generated from these files:
 	google/protobuf/compiler/plugin.proto
@@ -12,7 +12,7 @@ It has these top-level messages:
 	CodeGeneratorRequest
 	CodeGeneratorResponse
 */
-package google_protobuf_compiler
+package plugin_go
 
 import proto "github.com/golang/protobuf/proto"
 import math "math"
