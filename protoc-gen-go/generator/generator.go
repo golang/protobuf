d20896fc31fb0e955e57970d61eb6eea054ff048
David Symonds <dsymonds@golang.org>
2016-02-10 10:39:21 +1100

    Add support for plumbing json_name from protoc through to proto.Properties.
    This will be used by the jsonpb package at a later date.

--- protoc-gen-go/generator/generator.go
+++ protoc-gen-go/generator/generator.go
@@ -1517,6 +1517,11 @@ func (g *Generator) goTag(message *Descriptor, field *descriptor.FieldDescriptor
 			name = name[i+1:]
 		}
 	}
+	if json := field.GetJsonName(); json != "" && json != name {
+		// TODO: escaping might be needed, in which case
+		// perhaps this should be in its own "json" tag.
+		name += ",json=" + json
+	}
 	name = ",name=" + name
 	if message.proto3() {
 		// We only need the extra tag for []byte fields;
