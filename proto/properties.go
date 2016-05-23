e51d002c610dbe8c136679a67a6ded5df4d49b5c
matloob@google.com <matloob@golang.org>
2016-05-23 09:09:04 -0400

    net/proto2: remove <message>.ExtensionMap() from generated messages
    Turn generated message struct field XXX_Extensions map[int32]Extension
    into an embedded proto.InternalExtensions  struct
    
    InternalExtensions is a struct without any exported fields and methods.
    This effectively makes the representation of the extension map private.
    The proto package can access InternalExtensions by checking that the
    generated struct has the method 'extmap() proto.InternalExtensions'.
    
    Also lock accesses to the extension map.
    
    This change bumps the Go protobuf generated code version number. Any
    .pb.go files generated with this version of the proto package or later
    will require this version or later of the proto package to compile.

--- proto/properties.go
+++ proto/properties.go
@@ -682,7 +682,8 @@ func getPropertiesLocked(t reflect.Type) *StructProperties {
 	propertiesMap[t] = prop
 
 	// build properties
-	prop.extendable = reflect.PtrTo(t).Implements(extendableProtoType)
+	prop.extendable = reflect.PtrTo(t).Implements(extendableProtoType) ||
+		reflect.PtrTo(t).Implements(extendableProtoV1Type)
 	prop.unrecField = invalidField
 	prop.Prop = make([]*Properties, t.NumField())
 	prop.order = make([]int, t.NumField())
@@ -693,12 +694,15 @@ func getPropertiesLocked(t reflect.Type) *StructProperties {
 		name := f.Name
 		p.init(f.Type, name, f.Tag.Get("protobuf"), &f, false)
 
-		if f.Name == "XXX_extensions" { // special case
+		if f.Name == "XXX_InternalExtensions" { // special case
+			p.enc = (*Buffer).enc_exts
+			p.dec = nil // not needed
+			p.size = size_exts
+		} else if f.Name == "XXX_extensions" { // special case
 			p.enc = (*Buffer).enc_map
 			p.dec = nil // not needed
 			p.size = size_map
-		}
-		if f.Name == "XXX_unrecognized" { // special case
+		} else if f.Name == "XXX_unrecognized" { // special case
 			prop.unrecField = toField(&f)
 		}
 		oneof := f.Tag.Get("protobuf_oneof") // special case
