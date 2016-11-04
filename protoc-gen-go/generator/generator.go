da116c3771bf4a398a43f44e069195ef1c9688ef
lmilewski <lmilewski@google.com>
2016-11-04 18:21:45 +0000

    proto: generate Get methods for fields of basic type in proto3
    Given proto3 schema:
    
      message Foo {
        string some_value = 1;
      }
    
      message Bar {
        Foo foo = 1;
      }
    
      message Baz {
        Bar bar = 1;
      }
    
    Accessing baz.bar.foo.SomeValue currently requires this code:
    
            if f := baz.GetBar().GetFoo(); f != nil {
                    fmt.Printf("value was %v\n", f.SomeValue)
            }
    
    After the CL, it becomes:
    
            fmt.Printf("value was %v\n", baz.GetBar().GetFoo().GetSomeValue())
    
    There are downsides to generating more code:
       - all things equal, less code is better
       - larger API surface
    
    The upsides are:
     - consistency with proto2 API (proto2 and proto3 can be mixed. Keeping API consistent is important)
     - easier to explain API - all fields have Get method. No exceptions.
     - reduced user confusion - questions about this come up frequently
    
    testing:
      - added a new test
      - inspected a diff for an arbitrarily chosen schema
    PiperOrigin-RevId: 138212965

--- protoc-gen-go/generator/generator.go
+++ protoc-gen-go/generator/generator.go
@@ -2076,11 +2076,6 @@ func (g *Generator) generateMessage(message *Descriptor) {
 			star = "*"
 		}
 
-		// In proto3, only generate getters for message fields and oneof fields.
-		if message.proto3() && *field.Type != descriptor.FieldDescriptorProto_TYPE_MESSAGE && !oneof {
-			continue
-		}
-
 		// Only export getter symbols for basic types,
 		// and for messages and enums in the same package.
 		// Groups are not exported.
@@ -2139,7 +2134,11 @@ func (g *Generator) generateMessage(message *Descriptor) {
 			continue
 		}
 		if !oneof {
-			g.P("if m != nil && m." + fname + " != nil {")
+			if message.proto3() {
+				g.P("if m != nil {")
+			} else {
+				g.P("if m != nil && m." + fname + " != nil {")
+			}
 			g.In()
 			g.P("return " + star + "m." + fname)
 			g.Out()
