d40cbb787fd4581fc25ba328634b30c5e8a5f95d
Duco van Amstel <duco@improbable.io>
2017-05-17 11:45:09 +0100

    Add LogFields and ExtractRequestFields methods to protobuf.Duration.

---
+++ ptypes/duration/duration.hacks.pb.go
@@ -0,0 +1,8 @@
+package duration
+
+func (this *Duration) LogFields() map[string]string {
+	return map[string]string{}
+}
+
+func (this *Duration) ExtractRequestFields(dst map[string]interface{}) {
+}
