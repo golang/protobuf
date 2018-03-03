d8f920484501e9455474864940e27a785d7b512c
Damien Neil <dneil@google.com>
2018-03-02 16:18:49 -0800

    protoc-gen-go: add paths=source_relative option
    When --go_out=paths=source_relative:., output filenames are always
    derived from the input filenames. For example, the output file
    for a/b/c.proto will always be a/b/c.pb.go, regardless of the
    presence or absence of a go_package option in the input file.
    
    Fixes #515.

--- protoc-gen-go/generator/generator.go
+++ protoc-gen-go/generator/generator.go
@@ -321,12 +321,16 @@ func (d *FileDescriptor) goPackageName() (name string, explicit bool) {
 }
 
 // goFileName returns the output name for the generated Go file.
-func (d *FileDescriptor) goFileName() string {
+func (d *FileDescriptor) goFileName(pathType pathType) string {
 	name := *d.Name
 	if ext := path.Ext(name); ext == ".proto" || ext == ".protodevel" {
 		name = name[:len(name)-len(ext)]
 	}
 	name += ".pb.go"
+
+	if pathType == pathTypeSourceRelative {
+		return name
+	}
 
 	// Does the file have a "go_package" option?
 	// If it does, it may override the filename.
@@ -581,10 +585,18 @@ type Generator struct {
 	typeNameToObject map[string]Object          // Key is a fully-qualified name in input syntax.
 	init             []string                   // Lines to emit in the init function.
 	indent           string
+	pathType         pathType // How to generate output filenames.
 	writeOutput      bool
 	annotateCode     bool                                       // whether to store annotations
 	annotations      []*descriptor.GeneratedCodeInfo_Annotation // annotations to store
 }
+
+type pathType int
+
+const (
+	pathTypeImport pathType = iota
+	pathTypeSourceRelative
+)
 
 // New creates a new generator and allocates the request and response protobufs.
 func New() *Generator {
@@ -630,6 +642,15 @@ func (g *Generator) CommandLineParameters(parameter string) {
 			g.ImportPrefix = v
 		case "import_path":
 			g.PackageImportPath = v
+		case "paths":
+			switch v {
+			case "import":
+				g.pathType = pathTypeImport
+			case "source_relative":
+				g.pathType = pathTypeSourceRelative
+			default:
+				g.Fail(fmt.Sprintf(`Unknown path type %q: want "import" or "source_relative".`, v))
+			}
 		case "plugins":
 			pluginList = v
 		case "annotate_code":
@@ -1218,7 +1239,7 @@ func (g *Generator) GenerateAllFiles() {
 		if !g.writeOutput {
 			continue
 		}
-		fname := file.goFileName()
+		fname := file.goFileName(g.pathType)
 		g.Response.File = append(g.Response.File, &plugin.CodeGeneratorResponse_File{
 			Name:    proto.String(fname),
 			Content: proto.String(g.String()),
@@ -1227,7 +1248,7 @@ func (g *Generator) GenerateAllFiles() {
 			// Store the generated code annotations in text, as the protoc plugin protocol requires that
 			// strings contain valid UTF-8.
 			g.Response.File = append(g.Response.File, &plugin.CodeGeneratorResponse_File{
-				Name:    proto.String(file.goFileName() + ".meta"),
+				Name:    proto.String(file.goFileName(g.pathType) + ".meta"),
 				Content: proto.String(proto.CompactTextString(&descriptor.GeneratedCodeInfo{Annotation: g.annotations})),
 			})
 		}
@@ -1430,8 +1451,10 @@ func (g *Generator) generateImports() {
 		if fd.PackageName() == g.packageName {
 			continue
 		}
-		filename := fd.goFileName()
 		// By default, import path is the dirname of the Go filename.
+		// Use pathTypeImport here rather than whatever the user
+		// specified, since we do want the import path.
+		filename := fd.goFileName(pathTypeImport)
 		importPath := path.Dir(filename)
 		if substitution, ok := g.ImportMap[s]; ok {
 			importPath = substitution
