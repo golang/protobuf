22c36ed9548020eb693da7257ae6fecf2d563552
Joe Tsai <joetsai@digital-static.net>
2019-02-28 13:27:29 -0800

    protoapi: add CompressGZIP helper function
    The current generated API relies on GZIP since the Descriptor and
    EnumDescriptor methods return the file descriptor as the GZIP-compressed
    form of the raw encoded bytes.
    
    It turns out that it was a mistake to compress the descriptor since they
    hardly contribute any significant size to the total binary bloat and
    instead forces us to have a dependency on gzip in the public API.
    
    The CompressGZIP function added is hand-tailored to be as simple and fast
    as possible to reduce runtime cost as well.
    
    Change-Id: I32a62f8ba39a0f2dd12fb31800f270b672facf5f
    Reviewed-on: https://go-review.googlesource.com/c/164637
    Reviewed-by: Damien Neil <dneil@google.com>

--- protoapi/impl.go
+++ protoapi/impl.go
@@ -5,8 +5,11 @@
 package protoapi
 
 import (
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
+	"hash/crc32"
+	"math"
 	"strconv"
 )
 
@@ -47,6 +50,41 @@ func UnmarshalJSONEnum(m map[string]int32, data []byte, enumName string) (int32,
 		return 0, fmt.Errorf("cannot unmarshal %#q into enum %s", data, enumName)
 	}
 	return val, nil
+}
+
+// CompressGZIP compresses the input as a GZIP-encoded file.
+// The current implementation does no compression.
+func CompressGZIP(in []byte) (out []byte) {
+	// RFC 1952, section 2.3.1.
+	var gzipHeader = [10]byte{0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff}
+
+	// RFC 1951, section 3.2.4.
+	var blockHeader [5]byte
+	const maxBlockSize = math.MaxUint16
+	numBlocks := 1 + len(in)/maxBlockSize
+
+	// RFC 1952, section 2.3.1.
+	var gzipFooter [8]byte
+	binary.LittleEndian.PutUint32(gzipFooter[0:4], crc32.ChecksumIEEE(in))
+	binary.LittleEndian.PutUint32(gzipFooter[4:8], uint32(len(in)))
+
+	// Encode the input without compression using raw DEFLATE blocks.
+	out = make([]byte, 0, len(gzipHeader)+len(blockHeader)*numBlocks+len(in)+len(gzipFooter))
+	out = append(out, gzipHeader[:]...)
+	for blockHeader[0] == 0 {
+		blockSize := maxBlockSize
+		if blockSize > len(in) {
+			blockHeader[0] = 0x01 // final bit per RFC 1951, section 3.2.3.
+			blockSize = len(in)
+		}
+		binary.LittleEndian.PutUint16(blockHeader[1:3], uint16(blockSize)^0x0000)
+		binary.LittleEndian.PutUint16(blockHeader[3:5], uint16(blockSize)^0xffff)
+		out = append(out, blockHeader[:]...)
+		out = append(out, in[:blockSize]...)
+		in = in[blockSize:]
+	}
+	out = append(out, gzipFooter[:]...)
+	return out
 }
 
 // TODO: Remove this when v2 textpb is available.
