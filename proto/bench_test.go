42cfff4a76269831a3d06bdfffe2650040fe1ec2
Damien Neil <dneil@google.com>
2019-06-21 10:42:31 -0700

    benchmarks: add general-purpose benchmarks directory
    Move the benchmarks using the common protobuf datasets out of proto/ and
    into their own directory. Add benchmarks for text and JSON.
    
    Move initialization out of the Benchmark function to avoid including it
    in CPU/memory profiles.
    
    We could put benchmarks in each individual package (proto, prototext,
    etc.), but the need for common infrastructure around managing the test
    data makes it simpler to keep the benchmarks together. Also, it's nice
    to have a one-stop overview of performance.
    
    Change-Id: I17c37efb91b2413fc43ab1b4c35bff2e1330bc0a
    Reviewed-on: https://go-review.googlesource.com/c/protobuf/+/183245
    Reviewed-by: Joe Tsai <thebrokentoaster@gmail.com>

--- proto/bench_test.go
+++ proto/bench_test.go
@@ -7,25 +7,11 @@ package proto_test
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"path/filepath"
 	"reflect"
-	"strings"
 	"testing"
 
 	protoV1 "github.com/golang/protobuf/proto"
 	"google.golang.org/protobuf/proto"
-	pref "google.golang.org/protobuf/reflect/protoreflect"
-	preg "google.golang.org/protobuf/reflect/protoregistry"
-
-	benchpb "google.golang.org/protobuf/internal/testprotos/benchmarks"
-	_ "google.golang.org/protobuf/internal/testprotos/benchmarks/datasets/google_message1/proto2"
-	_ "google.golang.org/protobuf/internal/testprotos/benchmarks/datasets/google_message1/proto3"
-	_ "google.golang.org/protobuf/internal/testprotos/benchmarks/datasets/google_message2"
-	_ "google.golang.org/protobuf/internal/testprotos/benchmarks/datasets/google_message3"
-	_ "google.golang.org/protobuf/internal/testprotos/benchmarks/datasets/google_message4"
 )
 
 // The results of these microbenchmarks are unlikely to correspond well
@@ -85,100 +71,5 @@ func BenchmarkDecode(b *testing.B) {
 				})
 			})
 		}
-	}
-}
-
-// BenchmarkData runs various benchmarks using the general-purpose protocol buffer
-// benchmarking dataset:
-// https://github.com/protocolbuffers/protobuf/tree/master/benchmarks
-func BenchmarkData(b *testing.B) {
-	out, err := exec.Command("git", "rev-parse", "--show-toplevel").CombinedOutput()
-	if err != nil {
-		b.Fatal(err)
-	}
-	repoRoot := strings.TrimSpace(string(out))
-	dataDir := filepath.Join(repoRoot, ".cache", "benchdata")
-
-	var datasets []string
-	filepath.Walk(dataDir, func(path string, _ os.FileInfo, _ error) error {
-		if filepath.Ext(path) == ".pb" {
-			datasets = append(datasets, path)
-		}
-		return nil
-	})
-
-	for _, data := range datasets {
-		raw, err := ioutil.ReadFile(data)
-		if err != nil {
-			b.Fatal(err)
-		}
-		ds := &benchpb.BenchmarkDataset{}
-		if err := proto.Unmarshal(raw, ds); err != nil {
-			b.Fatal(err)
-		}
-		mt, err := preg.GlobalTypes.FindMessageByName(pref.FullName(ds.MessageName))
-		if err != nil {
-			b.Fatal(err)
-		}
-		var messages []proto.Message
-		for _, payload := range ds.Payload {
-			m := mt.New().Interface()
-			if err := proto.Unmarshal(payload, m); err != nil {
-				b.Fatal(err)
-			}
-			messages = append(messages, m)
-		}
-
-		var (
-			unmarshal = proto.Unmarshal
-			marshal   = proto.Marshal
-			size      = proto.Size
-		)
-		if *benchV1 {
-			unmarshal = func(b []byte, m proto.Message) error {
-				return protoV1.Unmarshal(b, m.(protoV1.Message))
-			}
-			marshal = func(m proto.Message) ([]byte, error) {
-				return protoV1.Marshal(m.(protoV1.Message))
-			}
-			size = func(m proto.Message) int {
-				return protoV1.Size(m.(protoV1.Message))
-			}
-		}
-
-		b.Run(filepath.Base(data), func(b *testing.B) {
-			b.Run("Unmarshal", func(b *testing.B) {
-				b.RunParallel(func(pb *testing.PB) {
-					for pb.Next() {
-						for _, p := range ds.Payload {
-							m := mt.New().Interface()
-							if err := unmarshal(p, m); err != nil {
-								b.Fatal(err)
-							}
-						}
-					}
-				})
-			})
-			b.Run("Marshal", func(b *testing.B) {
-				b.RunParallel(func(pb *testing.PB) {
-					for pb.Next() {
-						for _, m := range messages {
-							if _, err := marshal(m); err != nil {
-								b.Fatal(err)
-							}
-						}
-					}
-				})
-			})
-			b.Run("Size", func(b *testing.B) {
-				b.RunParallel(func(pb *testing.PB) {
-					for pb.Next() {
-						for _, m := range messages {
-							size(m)
-						}
-					}
-				})
-			})
-		})
 	}
 }
