96fb6f49c14283948f8e8a7f92b3aaddef237b32
Damien Neil <dneil@google.com>
2020-02-26 10:24:20 -0800

    ptypes: regenerate with v3.11.4
    Update .proto files to github.com/protocolbuffers v3.11.4.

--- ptypes/duration/duration.pb.go
+++ ptypes/duration/duration.pb.go
@@ -41,7 +41,7 @@ const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 //     if (duration.seconds < 0 && duration.nanos > 0) {
 //       duration.seconds += 1;
 //       duration.nanos -= 1000000000;
-//     } else if (durations.seconds > 0 && duration.nanos < 0) {
+//     } else if (duration.seconds > 0 && duration.nanos < 0) {
 //       duration.seconds -= 1;
 //       duration.nanos += 1000000000;
 //     }
